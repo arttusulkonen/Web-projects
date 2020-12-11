f232ac20cb95a1d66915560f51ca3fdcde0a4fbc
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2020-12-11 13:28:17 +0200

    added post-component and post-form-compoonent.  + interface Post {title, text, id?}

--- Blog/src/app/app.component.ts
+++ Blog/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Blog';
+  posts: Post[] = [
+    {title: 'I want to learn Angular components', text: 'I\'m still learning components..', id: 1},
+    {title: 'The next block', text: 'Will be, about directives and pipes', id: 2}
+  ];
 }
