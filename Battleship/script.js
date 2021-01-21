5d3ec19ec08e1e371f62e996baaa5b9f33e9576b
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-21 15:51:01 +0200

    created battlefield, added css styles, and first functions

--- Battleship/script.js
+++ Battleship/script.js
@@ -1,3 +1,20 @@
+const view = {
+    displayMessage: function(msg){
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location){
+        let cell = document.getElementById(location);
+        cell.classList.add('hit');
+    },
+    displayMiss: function(location){
+        let cell = document.getElementById(location);
+        cell.classList.add('miss');
+    }
+}
 
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMessage("Tap, tap, is this thing on?");
 
 
