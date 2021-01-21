b3bbfd0ba728a5afa2ce67b3e5244aa517ce4409
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-21 16:28:50 +0200

    ship model, added ships, fire function

--- Battleship/script.js
+++ Battleship/script.js
@@ -13,8 +13,59 @@ const view = {
     }
 }
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMessage("Tap, tap, is this thing on?");
+const model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+ships: [{ locations: ['10', '20', '30'], hits: ['','','']},
+        { locations: ['32', '33', '34'], hits: ['','','']},
+        { locations: ['63', '64', '65'], hits: ['','','']}],
+
+fire: function(guess){
+        for(let i = 0; i < this.numShips; i++){
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if(index >= 0){
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if(this.isSunk(ship)){
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMessage(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship){
+        for(let i = 0; i < this.shipLength; i++){
+            if(ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+model.fire("10");
+model.fire("20");
+model.fire("30");
+model.fire("32");
+model.fire("33");
+model.fire("34");
+
+
+
+
+
+
+
+
+
 
 
