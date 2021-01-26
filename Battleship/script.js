3ea541f94428577f6397b18e5a0e2f4cb4a1927e
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-26 13:03:23 +0200

    fireButton is work, also added a check for the correctness of shots

--- Battleship/script.js
+++ Battleship/script.js
@@ -52,12 +52,54 @@ fire: function(guess){
     }
 };
 
-model.fire("10");
-model.fire("20");
-model.fire("30");
-model.fire("32");
-model.fire("33");
-model.fire("34");
+function parseGuess(guess){
+    const alphabet = ["A","B","C","D","E","F","G"];
+
+    if(guess === null || guess.length !== 2){
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if(isNaN(row) || isNaN(column)){
+            alert("Oops, that isn't on the board.");
+        } else if(row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+        return null;
+    }
+}
+
+let controller = {
+    guesses: 0,
+
+    processGuess: function(guess){
+        let location = parseGuess(guess);
+        if(location){
+            this.guesses++;
+            let hit = model.fire(location);
+            if(hit && model.shipsSunk === model.numShips){
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
+    }
+}
+
+function handleFireButton(){
+    const guessInput = document.querySelector('#guessInput');
+    let guess = guessInput.value;
+    controller.processGuess(guess);
+    guessInput.value = '';
+}
+function init(){
+    const fireButton = document.querySelector('#fireButton');
+    fireButton.addEventListener('click', handleFireButton);
+}
+
+init();
 
 
 
